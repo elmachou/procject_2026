53adb1e941e81396411e32f94ed7c6d3a2e7aff0
EC <small82222@gmail.com>
2026-06-06 10:27:39 +0800

    update

--- 06JUN/lesson1.py
+++ 06JUN/lesson1.py
@@ -1,4 +1,9 @@
-file = open("student.txt","r",encoding="utf-8")
+from pathlib import Path
+
+BASE_DIR = Path(__file__).resolve().parent.parent
+txt_path = BASE_DIR / "student.txt"
+
+file = open(txt_path, "r", encoding="utf-8")
 print(type(file))
 content = file.read()
 print(content)
@@ -7,15 +12,21 @@ file.closed
 
 #=======================
 
-with open("student.txt","r",encoding="utf-8") as file:
+with open(txt_path, "r", encoding="utf-8") as file:
 	content = file.read()
 
 print(file.closed)
 
 #==============
+from pathlib import Path
 import csv
 
-with open("考試分數_3年6班.csv", "r", encoding="utf-8") as file:
+BASE_DIR = Path(__file__).resolve().parent.parent
+csv_path = BASE_DIR / "考試分數_3年6班.csv"
+
+with open(csv_path, "r", encoding="utf-8") as file:
     reader = csv.DictReader(file)
+    print(type(reader))
     for row in reader:
-        print(row)
+        if int(row["數學"]) > 90:
+            print(row["學生姓名"])

28b08371ac785ae1e7b5fa0c2e3aaf94b4f41ab4
EC <small82222@gmail.com>
2026-06-06 11:09:06 +0800

    update

--- 06JUN/lesson1.py
+++ 06JUN/lesson1.py
@@ -1,7 +1,7 @@
 from pathlib import Path
 
-BASE_DIR = Path(__file__).resolve().parent.parent
-txt_path = BASE_DIR / "student.txt"
+BASE_DIR_TEXT = Path(__file__).resolve().parent.parent
+txt_path = BASE_DIR_TEXT / "student.txt"
 
 file = open(txt_path, "r", encoding="utf-8")
 print(type(file))
@@ -18,11 +18,10 @@ with open(txt_path, "r", encoding="utf-8") as file:
 print(file.closed)
 
 #==============
-from pathlib import Path
 import csv
 
-BASE_DIR = Path(__file__).resolve().parent.parent
-csv_path = BASE_DIR / "考試分數_3年6班.csv"
+BASE_DIR_CSV = Path(__file__).resolve().parent.parent
+csv_path = BASE_DIR_CSV / "考試分數_3年6班.csv"
 
 with open(csv_path, "r", encoding="utf-8") as file:
     reader = csv.DictReader(file)
---
+++ 06JUN/lesson2.py
@@ -0,0 +1,8 @@
+#def define a function
+def main():
+    print("main function的命名空間")
+    print(n)
+
+if __name__ == "__main__":
+    n = 10
+    main() #呼叫main函式
